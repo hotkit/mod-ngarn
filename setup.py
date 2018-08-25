8d2f51e5bce28ee7b5e303f6b18cc99b60f92a97
Tle Ekkul <aryuth.ekkul@proteus-tech.com>
2018-08-25 20:22:15 +0700

    use underscore in package name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(
-    name='mod-ngarn',
+    name='mod_ngarn',
     version='0.0.1',
     description='Simple async worker jobs',
     author='Proteus Tech',
