6e0796d3b7c703d5f6920c6dab777f36025afa75
Tle Ekkul <aryuth.ekkul@proteus-tech.com>
2018-08-25 13:56:08 +0700

    Can auto decode JSON

--- Python/connection.py
+++ Python/connection.py
@@ -1,10 +1,18 @@
 import asyncpg
 import os
+import json
 
 async def get_connection():
     PGDBNAME = os.getenv('PGDBNAME')
     PGHOST = os.getenv('PGHOST')
     PGPASSWORD = os.getenv('PGPASSWORD')
     PGUSER = os.getenv('PGUSER')
-    return await asyncpg.connect(user=PGUSER, password=PGPASSWORD,
+    cnx = await asyncpg.connect(user=PGUSER, password=PGPASSWORD,
         database=PGDBNAME, host=PGHOST)
+    await cnx.set_type_codec(
+            'json',
+            encoder=json.dumps,
+            decoder=json.loads,
+            schema='pg_catalog'
+        )
+    return cnx
